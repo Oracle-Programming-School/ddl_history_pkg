676a91b7c49de849e466b0c5fb7c40f19832118e
Muhammad Faisal <muhammadfaisal1521@gmail.com>
2023-03-11 00:43:05 +0500

    Update ddl_history_pkg.sql

--- ddl_history_pkg.sql
+++ ddl_history_pkg.sql
@@ -1,5 +1,4 @@
-
-CREATE OR REPLACE PACKAGE ddl_history_pkg AS
+CREATE OR REPLACE PACKAGE PROJECT_ADMIN.ddl_history_pkg AS
   
 -- adds a new entry to the DDL history table
   PROCEDURE add_ddl_history(p_object_type VARCHAR2, p_object_name VARCHAR2);
@@ -11,21 +10,36 @@ END ddl_history_pkg;
 /
 
 
-CREATE OR REPLACE PACKAGE BODY ddl_history_pkg AS
+CREATE OR REPLACE PACKAGE BODY PROJECT_ADMIN.ddl_history_pkg AS
 
-  PROCEDURE add_ddl_history(p_object_type VARCHAR2, p_object_name VARCHAR2) IS
-    l_ddl_text CLOB;
-  BEGIN
-    -- retrieve the DDL for the specified object
-    SELECT dbms_metadata.get_ddl(UPPER(p_object_type), UPPER(p_object_name))
-      INTO l_ddl_text
-      FROM dual;
+PROCEDURE add_ddl_history(p_object_type VARCHAR2, p_object_name VARCHAR2) IS
+  l_ddl_text CLOB;
+  l_database_name VARCHAR2(100);
+  l_object_version NUMBER;
+BEGIN
+  -- retrieve the DDL for the specified object
+  SELECT dbms_metadata.get_ddl(p_object_type, p_object_name)
+    INTO l_ddl_text
+    FROM dual;
+
+  -- retrieve the current database name and replace symbols with underscores
+  SELECT REPLACE(TRANSLATE(global_name, '!@#$%^&*()+=}{[]|\:;"<>,.?/~`-', '_____________________'), '_', '')
+    INTO l_database_name
+    FROM global_name;
+
+  -- generate the object version
+  SELECT count(*)+1
+    INTO l_object_version
+    FROM ddl_history
+    WHERE object_type = p_object_type
+    AND object_name = p_object_name;
     
-    -- store the DDL in the ddl_history table
-    INSERT INTO ddl_history (object_type, object_name, ddl_text) 
-      VALUES (UPPER(p_object_type), UPPER(p_object_name), l_ddl_text);
-  END add_ddl_history;
-  
+  -- store the DDL in the ddl_history table
+  INSERT INTO ddl_history (object_type, object_name, ddl_text, database_name, object_version) 
+    VALUES (p_object_type, p_object_name, l_ddl_text, l_database_name, l_object_version);
+END add_ddl_history;
+
+
   PROCEDURE truncate_history(p_object_type VARCHAR2, p_object_name VARCHAR2, p_days NUMBER DEFAULT NULL) IS
     l_date DATE;
   BEGIN
@@ -48,3 +62,4 @@ CREATE OR REPLACE PACKAGE BODY ddl_history_pkg AS
 
 END ddl_history_pkg;
 /
+
